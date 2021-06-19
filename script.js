b46def9dba538d1005b10c68ebc988f2c9ed337c
sudoer777 <78781902+sudoer777@users.noreply.github.com>
2021-06-18 18:27:41 -0600

    Create playRound() function

--- script.js
+++ script.js
@@ -12,3 +12,35 @@ function computerPlay() {
     else if(num == 1) return "Paper";
     else if(num == 2) return "Scissors";
 }
+
+function playRound(playerSelection, computerSelection)
+{
+    /*
+    Change playerSelection and computerSelection to all lowercase and capitalize first letter
+    Check if playerSelection and computerSelection are valid values
+    Compare, and return who won
+    */
+
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.substring(1).toLowerCase();
+    computerSelection = computerSelection[0].toUpperCase() + computerSelection.substring(1).toLowerCase();
+
+    // Check for invalid value
+    if ((playerSelection != "Rock" && playerSelection != "Paper" && playerSelection != "Scissors")
+                || (computerSelection != "Rock" && computerSelection != "Paper" && computerSelection != "Scissors")) {
+        return "Error: invalid selection!";
+    }
+
+    // Tie
+    if(playerSelection == computerSelection) return "You tied!"
+
+    let playerWins = false;
+    // Check if player wins
+    if((playerSelection == "Rock" && computerSelection == "Scissors")
+                || (playerSelection == "Paper" && computerSelection == "Rock")
+                || (playerSelection == "Scissors" && computerSelection == "Paper"))
+        playerWins = true;
+
+    // Return end game result
+    if(playerWins) return "You Win! " + playerSelection + " beats " + computerSelection;
+    else return "You Lose! " + computerSelection + " beats " + playerSelection;
+}
